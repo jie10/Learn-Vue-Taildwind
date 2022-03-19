6d0a3d074263c20dabcf0d3753fd3c71e4fd8f07
Benjie Jabir Burillo <benjieburillo10@gmail.com>
2022-03-20 00:09:25 +0800

    Update AddResource.vue

--- src/components/learning-resources/AddResource.vue
+++ src/components/learning-resources/AddResource.vue
@@ -5,7 +5,7 @@
         <label class="block text-sm font-medium text-gray-700" for="title">Title</label>
         <div class="mt-1">
           <input id="title"
-                 class="form-input shadow-sm focus:ring-indigo-500 md:w-full sm:w-full xl:w-72 focus:border-indigo-500 block  sm:text-sm border-gray-300 rounded-md"
+                 class="form-input shadow-sm focus:ring-indigo-500 w-full focus:border-indigo-500 block  sm:text-sm border-gray-300 rounded-md"
                  name="title"
                  placeholder="Enter your title"
                  type="text"/>
@@ -14,18 +14,18 @@
       <div>
         <label class="block text-sm font-medium text-gray-700" for="description">Description</label>
         <div class="mt-1">
-          <input id="description"
-                 class="form-input shadow-sm focus:ring-indigo-500 sm:w-full focus:border-indigo-500 block w-1/4 sm:text-sm border-gray-300 rounded-md"
-                 name="description"
-                 placeholder="Enter your description"
-                 type="text"/>
+          <textarea id="description"
+                    class="form-input shadow-sm focus:ring-indigo-500 w-full focus:border-indigo-500 block w-1/4 sm:text-sm border-gray-300 rounded-md"
+                    name="description"
+                    placeholder="Enter your description"
+                    type="text"/>
         </div>
       </div>
       <div>
         <label class="block text-sm font-medium text-gray-700" for="link">Link</label>
         <div class="mt-1">
           <input id="link"
-                 class="form-input shadow-sm focus:ring-indigo-500 sm:w-full focus:border-indigo-500 block w-1/4 sm:text-sm border-gray-300 rounded-md"
+                 class="form-input shadow-sm focus:ring-indigo-500 w-full focus:border-indigo-500 block w-1/4 sm:text-sm border-gray-300 rounded-md"
                  name="link"
                  placeholder="Enter your link"
                  type="text"/>
